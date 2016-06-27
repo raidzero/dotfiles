8dc1c1d23d9a2ea41104aa794d5400107b3c7cb7
raidzero <paul.raidzero@gmail.com>
2016-06-27 15:32:01 -0600

    fix new workspace bug

--- .config/i3/windowname.py
+++ .config/i3/windowname.py
@@ -15,38 +15,41 @@ workspace_defaults = {
 	10: "10:game"
 }
 
-i3 = i3ipc.Connection()
 
 def truncate_name(name):
+	length = 50
 	name = name.replace("&", "+")
-	truncated = name[:40] + (name[40:] and '...')
+	truncated = name[:length] + (name[length:] and '...')
 	return filter(lambda x: x in set(string.printable), truncated)
 
-
-def on_event(i3, e):
-	if e.change in ['focus', 'title']:
-		tree = i3.get_tree()
-		window = tree.find_focused();
-		if window != None:
-			new_name = window.name
-			workspace = window.workspace()
-			
-			if workspace != None:		
-				current_num = workspace.num
-				current_name = workspace.name
-			
-				for ws in tree.workspaces():
-					current_name = ws.name					
-					if ws.num != current_num:
-						new_name = workspace_defaults[ws.num]
-					else:	
-						if window.name != None:
-							new_name = "%d:%s" % (current_num, truncate_name(window.name))
-						else:
-							new_name = workspace_defaults[ws.num]
-								
-					i3.command("rename workspace \"%s\" to \"%s\"" % (current_name, new_name))
+def process_workspaces(tree):
+	# get currently focused window
+	window = tree.find_focused()
+	
+	# get its name and workspace
+	new_name = window.name
+	workspace = window.workspace()
+	
+	# operate on all workspaces in the tree
+	for ws in tree.workspaces():
+		current_name = ws.name
 		
+		# assume new name is the default
+		new_name = workspace_defaults[ws.num]
+		
+		# if we actually have a window name and ws is the current workspace, make a new name	
+		if ws != None and workspace != None and ws.num == workspace.num and window.name != None:
+			new_name = "%d:%s" % (workspace.num, truncate_name(window.name))
+		
+		# do the rename on this workspace
+		i3.command("rename workspace \"%s\" to \"%s\"" % (current_name, new_name))
+	
+def on_event(i3, e):
+	if e.change in ['focus', 'title', 'empty', 'init']:
+		process_workspaces(i3.get_tree())
+		
+i3 = i3ipc.Connection()
+
 i3.on('workspace', on_event)
 i3.on('window', on_event)
 
