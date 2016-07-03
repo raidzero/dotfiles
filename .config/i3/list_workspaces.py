4db476d834a7c4c7c3d28137867dc8e53972cfb1
raidzero <paul.raidzero@gmail.com>
2016-07-03 12:08:25 -0600

    updates

--- .config/i3/list_workspaces.py
+++ .config/i3/list_workspaces.py
@@ -2,7 +2,7 @@
 import i3ipc
 import sys
 
-focused_ws = " <span foreground='#00ff54' underline='single'>%s</span> "
+focused_ws = " <span foreground='#00ff54'>%s</span> "
 unfocused_ws = " %s "
 
 def printData(data):
@@ -15,12 +15,19 @@ i3 = i3ipc.Connection()
 # start off by printing current state
 workspaces = i3.get_workspaces()
 workspaceLine = ''
+numWorkspaces = len(workspaces)
 
+wsnum = 1
 for ws in workspaces:
+	divider = '|'
+	if wsnum == numWorkspaces:
+		divider = ''
+	
 	if ws['focused']:
-		workspaceLine += focused_ws % ws.name
+		workspaceLine += (focused_ws + divider) % ws.name
 	else:
-		workspaceLine += unfocused_ws % ws.name
+		workspaceLine += (unfocused_ws + divider) % ws.name
+	wsnum += 1
 
 printData(workspaceLine)
 	
@@ -30,16 +37,26 @@ def showWorkspaces(i3, e):
 	
 	workspaces = i3.get_workspaces()
 	workspaceLine = ''
+	numWorkspaces = len(workspaces)
+	wsnum = 1
 
 	for ws in workspaces:
+		divider = '|'
+		if wsnum == numWorkspaces:
+			divider = ''
 		if ws['focused']:
-			workspaceLine += focused_ws % ws.name
+			workspaceLine += (focused_ws + divider) % ws.name
 		else:
-			workspaceLine += unfocused_ws % ws.name
-	
+			workspaceLine += (unfocused_ws + divider) % ws.name
+		wsnum += 1
+
 	printData(workspaceLine)
 
-			
-i3.on('workspace', showWorkspaces)
 
-i3.main()
+if not len(sys.argv) > 1:			
+	i3.on('workspace', showWorkspaces)
+
+	i3.main()
+else:
+	sys.stdout.write(' ' + i3.get_tree().find_focused().name)
+	sys.stdout.flush()
