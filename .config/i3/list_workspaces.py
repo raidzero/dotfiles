eace8de6e8f43fe4673beddc8e602d29e72cbd00
raidzero <paul.raidzero@gmail.com>
2016-07-09 11:28:17 -0600

    yabar i3 mode & ws urgency display, updates

--- .config/i3/list_workspaces.py
+++ .config/i3/list_workspaces.py
@@ -3,7 +3,28 @@ import i3ipc
 import sys
 
 focused_ws = " <span foreground='#00ff54'>%s</span> "
+urgent_ws = " <span foreground='#ff0054'>%s</span> "
+
 unfocused_ws = " %s "
+
+def buildWorkspaceLine(workspaces):
+	workspaceLine = ''
+	numWorkspaces = len(workspaces)
+	wsnum = 1
+	
+	for ws in workspaces:
+		divider = '|'
+		if wsnum == numWorkspaces:
+			divider = ''
+	
+		if ws['focused']:
+			workspaceLine += (focused_ws + divider) % ws.name
+		elif ws['urgent']:
+			workspaceLine += (urgent_ws + divider) % ws.name
+		else:
+			workspaceLine += (unfocused_ws + divider) % ws.name
+	wsnum += 1
+	return workspaceLine
 
 def printData(data):
 	sys.stdout.flush()
@@ -14,49 +35,17 @@ i3 = i3ipc.Connection()
 
 # start off by printing current state
 workspaces = i3.get_workspaces()
-workspaceLine = ''
-numWorkspaces = len(workspaces)
-
-wsnum = 1
-for ws in workspaces:
-	divider = '|'
-	if wsnum == numWorkspaces:
-		divider = ''
-	
-	if ws['focused']:
-		workspaceLine += (focused_ws + divider) % ws.name
-	else:
-		workspaceLine += (unfocused_ws + divider) % ws.name
-	wsnum += 1
-
+workspaceLine = buildWorkspaceLine(workspaces)
 printData(workspaceLine)
 	
 def showWorkspaces(i3, e):
-	if not e.change in [ 'init', 'focus', 'empty', 'rename' ]:
+	if not e.change in [ 'init', 'focus', 'empty', 'rename', 'urgent' ]:
 		return 
 	
 	workspaces = i3.get_workspaces()
-	workspaceLine = ''
-	numWorkspaces = len(workspaces)
-	wsnum = 1
-
-	for ws in workspaces:
-		divider = '|'
-		if wsnum == numWorkspaces:
-			divider = ''
-		if ws['focused']:
-			workspaceLine += (focused_ws + divider) % ws.name
-		else:
-			workspaceLine += (unfocused_ws + divider) % ws.name
-		wsnum += 1
-
+	workspaceLine = buildWorkspaceLine(workspaces)
 	printData(workspaceLine)
 
 
-if not len(sys.argv) > 1:			
-	i3.on('workspace', showWorkspaces)
-
-	i3.main()
-else:
-	sys.stdout.write(' ' + i3.get_tree().find_focused().name)
-	sys.stdout.flush()
+i3.on('workspace', showWorkspaces)
+i3.main()
---
+++ .config/i3/mode.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import i3ipc
+import sys
+
+mode_disp = " <span foreground='#ff0054'>[%s]</span> "
+
+i3 = i3ipc.Connection()
+
+def showMode(i3, event):
+	mode = event.change
+	if mode != 'default':
+		sys.stdout.flush()
+		sys.stdout.write(mode_disp % mode)
+		sys.stdout.flush()
+	else:
+		sys.stdout.flush()
+		sys.stdout.write(' ')
+		sys.stdout.flush()
+
+i3.on('mode', showMode)
+i3.main()
