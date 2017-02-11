38edd72cef3c3aa138acbae8380e565a8d8b54f8
raidzero <paul.raidzero@gmail.com>
2017-02-10 22:46:58 -0700

    add whole i3 directory

--- .config/i3/i3status.sh
+++ .config/i3/i3status.sh
@@ -1,8 +1,13 @@
-#!/bin/sh
+#!/bin/bash
+#i3status | while :
+#do
+#    read line
+#    memline="MEM: `free -m | grep "^Mem:" | awk '{printf("%2.0f%%", (($2-$7)/$2)*100)}'`"
+#    echo "$memline | $line" || exit 1
+#done
 
 i3status | while :
 do
 	read line
 	echo $line > /tmp/i3status
-	sleep 2
 done
--- .config/i3/polybar.sh
+++ .config/i3/polybar.sh
@@ -6,4 +6,7 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
+# Launch bar1 and bar2
 polybar top &
+
+echo "Bars launched..."
