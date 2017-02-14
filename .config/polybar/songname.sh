6ed03c4735fac5db750e36824505c39589d3ab72
raidzero <paul.raidzero@gmail.com>
2017-02-14 14:26:26 -0700

    dont display song name if there is no song playing or paused. also just underline the individual polybar modules instead of the weird divider pipe thing

--- .config/polybar/songname.sh
+++ .config/polybar/songname.sh
@@ -3,10 +3,9 @@
 WINDOW_DATA=`wmctrl -l | grep "Google Play Music"`
 
 if [ -z "$WINDOW_DATA" ]; then
-	[ -z "$1" ] && echo "No music playing."
 	exit 1
 fi
 
 HOSTNAME=`hostname`
 
-echo "$WINDOW_DATA" | sed 's/0x[0-9a-f]* *[0-9]* *'"$HOSTNAME"' *//' | sed 's/- Google Play Music$//' | awk -F ' - ' '{print " |   " $2 "- "  $1}'
+echo "$WINDOW_DATA" | sed 's/0x[0-9a-f]* *[0-9]* *'"$HOSTNAME"' *//' | sed 's/- Google Play Music$//' | awk -F ' - ' '{if($2 != ""){print "  " $2 " - " $1}else{print ""}}'
