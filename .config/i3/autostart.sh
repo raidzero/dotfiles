0e3c04976e15e47e9669fd5f3c750b33457979ba
raidzero <paul.raidzero@gmail.com>
2017-03-06 21:35:42 -0700

    updates

--- .config/i3/autostart.sh
+++ .config/i3/autostart.sh
@@ -1,13 +1,13 @@
 urxvtd &
 compton -b --config ~/.config/compton/compton.conf &
-setxkbmap -option "caps:super" &
 dropbox start &
 sh ~/.fehbg & # set background
 ~/.config/i3/polybar.sh &
 urxvt -name urxvt_scratchpad -title ScratchPad &
 xautolock -locker ~/.config/i3/lockscreen -time 5 &
 
-#xfce daemons
-xfsettingsd &
+# fix keybaord shortcuts
+setxkbmap -model chromebook_m_ralt -layout us &
+
+# this is needed to make brightness keys work
 xfce4-power-manager &
-xfce4-volumed &
