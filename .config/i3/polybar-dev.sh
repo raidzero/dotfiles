c191f6b5c5c6289b85eb39b01cda8843cae82727
raidzero <paul.raidzero@gmail.com>
2017-05-08 08:04:47 -0600

    polybar dev launcher

---
+++ .config/i3/polybar-dev.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+if [ "$HOSTNAME" == "chromebook" ] || [ "$HOSTNAME" == "archvm" ]; then
+	CONFIG=~/.config/polybar/laptop
+else
+	CONFIG=~/.config/polybar/native
+fi
+
+# run the same bar on all monitors
+for OUTPUT in $(xrandr | grep " connected" | cut -d ' ' -f1); do
+	echo "MONITOR=$OUTPUT ~/Projects/polybar/build/bin/polybar -c $CONFIG top &"
+	MONITOR="$OUTPUT" ~/Projects/polybar/build/bin/polybar -c $CONFIG top & 
+	echo "$CONFIG launched on $OUTPUT"
+done
+
