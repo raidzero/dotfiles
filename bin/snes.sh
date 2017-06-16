71f5cf331c05f64f5959e4b233c4a4d39f356e4f
raidzero <paul.raidzero@gmail.com>
2017-06-16 10:48:56 -0600

    whoops broke metroid snes9x launcher

--- bin/snes.sh
+++ bin/snes.sh
@@ -3,14 +3,16 @@
 METROID=$1
 
 if [ -n "$METROID" ]; then
-	ROMS=$(ls ~/roms/SNES/metroid | grep "\.zip$" | sed 's/\.zip$//g')
+	ROMPATH="$HOME/roms/SNES/metroid"
 else
-	ROMS=$(ls ~/roms/SNES | grep "\.zip$" | sed 's/\.zip//g')
+	ROMPATH="$HOME/roms/SNES"
 fi
+
+ROMS=$(ls "$ROMPATH" | grep "\.zip$" | sed 's/\.zip//g')
 
 SELECTED=$(echo "$ROMS" | dmenu -i -nb '#000000' -nf '#ffffff' -sb '#ff00ff' -sf '#000000' -fn 'Inconsolata 11')
 
 echo "LAUNCHING:"
-ls -l "$HOME/roms/SNES/${SELECTED}.zip"
+ls -l "${ROMPATH}/${SELECTED}.zip"
 
-snes9x-gtk "$HOME/roms/SNES/${SELECTED}.zip"
+snes9x-gtk "${ROMPATH}/${SELECTED}.zip"
